6285ce3f50ddce0944ba8209e855950e3ca71867
FAITHY4556 <fcureg0148@mc-knoller.edu.ph>
2025-05-16 16:19:39 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll('.btn');
-
-buttons.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    const name = btn.getAttribute('data-name');
-    window.location.href = name + '.html';
-  });
-});
